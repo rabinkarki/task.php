12126a8f73dbd75aa9bd2447fa242ff87ad01637
rabinkarki <104356400+rabinkarki@users.noreply.github.com>
2022-05-17 23:15:45 -0700

    Update task.php

--- task.php
+++ task.php
@@ -2,30 +2,45 @@
 $students=[["name" => "rabin", "email" => "rabin.karki@hazesoft.co", "phone" => "9815349260", "class" => "higher", "course" => "bachelor" ],
 ["name" => "pawan", "email" => "rabinkarki@gmail.com", "phone" => "98826377232", "class" => "lower", "course" => "science" ] ];
 
+// $lens=count($students);
+    
+
+    function passarray($students){
+
+        echo "<table border='2px'> <tr>";
+
 
     
-    function passarray(){
-        global $students;
-        for($i=0;$i<10;$i++){
-            
-    if ( $students[1]["email"]=="rabinkarkigmail.com"){
-        echo "email is valid<br>";}
-        else{
-            echo "email is invalid <br>";
-            break;
-        }}
+ foreach($students as $key=>$value){
     
+    if (!filter_var($value['email'], FILTER_VALIDATE_EMAIL)) {
+        echo "email is invalid";
+    break;
+
+    }
+    
+            //  if( $value['email'] != 'rabin.karkigv@hazesoft.co' ){
+            //      break;
+            //  }
+            if($value['email'] == 'rabin.karki@hazesoft.co'){
+                echo "<td>".$value['name']."</td>";
+                echo "<td>".$value['email']."</td>";
+                echo "<td>".$value['phone']."</td>";
+                echo "<td>".$value['class']."</td>";
+                echo "<td>".$value['course']."</td>";     
+            }
+                //        else{
+                //  echo "email is incorrect";
+    
+            }
+            
+        
+        echo "</tr>";
+        echo "</table>";
+    }
+        
         
        
-    if($students[0]["email"]=="rabin.karki@hazesoft.co"){
-        echo "<pre>";
-        print_r($students[0]);
-        echo "</pre>";
+     passarray($students);
 
-    }
-        else{
-            echo "email is incorrect";
-        }
-    }
-    passarray();
 ?>
